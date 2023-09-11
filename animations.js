70efd1b079ffcb75854c1250c0aad1eb2d0786bc
Tony Callaghan <57832437+TonyCallaghan@users.noreply.github.com>
2023-09-11 22:22:31 +0100

    Create animations.js

---
+++ animations.js
@@ -0,0 +1,7 @@
+gsap.from(".gg:not(.gg--indent):not(.gg-last):not(.gg-symbol--8)", { duration: 0.7, x: 400, opacity: 0, stagger: 0.1 });
+gsap.from(".gg--indent", { duration: 0.7, x: 500, opacity: 0, delay: 0.5 });
+gsap.from(".Title", { duration: 0.7, x: 600, opacity: 0, delay: 0.75 });
+gsap.from(".Links", { duration: 0.7, x: 600, opacity: 0, delay: 1 });
+gsap.from(".gg-last", { duration: 0.7, x: 300, opacity: 0, delay: 0.25 });
+gsap.from(".gg-symbol--8", { duration: 0.7, x: 300, opacity: 0, delay: 0.2 });
+gsap.from(".Arrow", { duration: 0.7, y: -300, opacity: 0, delay: 1.25 });
